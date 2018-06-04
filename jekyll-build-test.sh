5aad327976184241cf036c37e75ad85d3c1abb41
Simon Mueller <s.mueller.hn@web.de>
2018-06-04 23:27:56 +0200

    change build prefix

--- jekyll-build-test.sh
+++ jekyll-build-test.sh
@@ -6,17 +6,17 @@ export GEM_HOME=$PWD/gems &&
 # prepare the deployment directory
 rm -rf ./deploy-test         &&
 mkdir ./deploy-test          && 
-mkdir ./deploy-test/german   && 
-mkdir ./deploy-test/english  && 
+mkdir ./deploy-test/de   && 
+mkdir ./deploy-test/en  && 
 
 # build german site 
 cd site-german          &&
 bundle install          &&
-bundle exec jekyll build --destination ../deploy-test/german --config _config.yml,_config_test.yml &&
+bundle exec jekyll build --destination ../deploy-test/de --config _config.yml,_config_test.yml &&
 cd ..                  # &&
 
 # build english site 
 #cd site-english          &&
 #bundle install           &&
-#bundle exec jekyll build --destination ../deploy-test/english --config _config.yml,_config_test.yml &&
+#bundle exec jekyll build --destination ../deploy-test/en --config _config.yml,_config_test.yml &&
 #cd ..
