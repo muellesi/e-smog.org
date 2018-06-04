a4ea0fae9a71c8fbd77cfc3d21bff85080c2755d
Simon Mueller <s.mueller.hn@web.de>
2018-06-04 23:38:07 +0200

    start integration to master

--- jekyll-build.sh
+++ jekyll-build.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-# first, build theme gem
-cd ./theme-e-smog       &&
-./build.sh              &&
-cd ..                   &&
+mkdir -p $PWD/gems         &&
+export GEM_HOME=$PWD/gems &&
 
 # prepare the deployment directory
 rm -rf ./deploy         &&
 mkdir ./deploy          && 
-mkdir ./deploy/german   && 
-mkdir ./deploy/english  && 
+mkdir ./deploy/de   && 
+mkdir ./deploy/en  && 
 
 # build german site 
 cd site-german          &&
 bundle install          &&
-bundle exec jekyll build --destination ../deploy/german --config _config.yml 
+bundle exec jekyll build --destination ../deploy/de --config _config.yml 
 
 #&& mkdir ./deploy/english &&
 #jekyll build --source ./site-english/ --destination ./deploy/english --config ./site-english/_config.yml &&
